730480c4404db025a5a7813e49b60a6794541831
OFFpolice <offpolice2077@gmail.com>
2025-11-12 22:49:26 +0200

    Update tg.js

--- static/js/tg.js
+++ static/js/tg.js
@@ -1,17 +1,15 @@
 window.addEventListener("load", () => {
-  // 1. Скрываем preloader
   document.body.classList.remove("loading");
 
-  // 2. Проверяем, что WebApp доступен
   if (window.Telegram && window.Telegram.WebApp) {
     const tg = window.Telegram.WebApp;
 
-    tg.ready();                         // Инициализация
-    tg.expand();                        // Расширяет WebApp на экран (можно убрать, если fullscreen)
-    tg.enableClosingConfirmation();     // Включает подтверждение закрытия
-    tg.disableVerticalSwipes();         // Блокирует вертикальные свайпы
-    tg.requestFullscreen();             // Запрос fullscreen
-    tg.lockOrientation();               // Блокирует ориентацию (обычно вертикально)
+    tg.ready();
+    tg.expand();
+    tg.enableClosingConfirmation();
+    tg.disableVerticalSwipes();
+    //tg.requestFullscreen();
+    tg.lockOrientation();
 
     console.log("Telegram WebApp is ready:", tg.initData);
   } else {
