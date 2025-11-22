952d6145b4121fcf6c47e34fa496e9f7586a8938
OFFpolice <offpolice2077@gmail.com>
2025-11-22 09:42:38 +0200

    Update tg.js

--- static/js/tg.js
+++ static/js/tg.js
@@ -1,22 +1,3 @@
-//window.addEventListener("load", () => {
-
-  //if (window.Telegram && window.Telegram.WebApp) {
-    //const tg = window.Telegram.WebApp;
-
-    //tg.ready();
-    //tg.expand();
-    //tg.enableClosingConfirmation();
-    //tg.disableVerticalSwipes();
-    //tg.requestFullscreen();
-    //tg.lockOrientation();
-
-    //console.log("Telegram WebApp is ready:", tg.initData);
-  //} else {
-    //console.warn("Telegram WebApp not found.");
-  //}
-//});
-
-
 window.addEventListener("load", () => {
 
   if (window.Telegram && window.Telegram.WebApp) {
@@ -30,17 +11,6 @@ window.addEventListener("load", () => {
     tg.lockOrientation();
 
     console.log("Telegram WebApp is ready:", tg.initData);
-
-    // --- ГЛАВНОЕ: обновляем высоты Mini App ---
-    document.documentElement.style.setProperty('--tg-viewport-height', `${tg.viewportHeight}px`);
-    document.documentElement.style.setProperty('--tg-viewport-stable-height', `${tg.viewportStableHeight}px`);
-
-    // Обновление при изменении размеров (потягивание Mini App вверх)
-    tg.onEvent('viewportChanged', () => {
-      document.documentElement.style.setProperty('--tg-viewport-height', `${tg.viewportHeight}px`);
-      document.documentElement.style.setProperty('--tg-viewport-stable-height', `${tg.viewportStableHeight}px`);
-    });
-
   } else {
     console.warn("Telegram WebApp not found.");
   }
