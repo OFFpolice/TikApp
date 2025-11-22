6f4787671192aae153fd748d9901d6a4b82042da
OFFpolice <offpolice2077@gmail.com>
2025-11-22 09:38:58 +0200

    Update tg.js

--- static/js/tg.js
+++ static/js/tg.js
@@ -1,20 +1,20 @@
 //window.addEventListener("load", () => {
 
-  if (window.Telegram && window.Telegram.WebApp) {
-    const tg = window.Telegram.WebApp;
+  //if (window.Telegram && window.Telegram.WebApp) {
+    //const tg = window.Telegram.WebApp;
 
-    tg.ready();
-    tg.expand();
-    tg.enableClosingConfirmation();
-    tg.disableVerticalSwipes();
-    tg.requestFullscreen();
-    tg.lockOrientation();
+    //tg.ready();
+    //tg.expand();
+    //tg.enableClosingConfirmation();
+    //tg.disableVerticalSwipes();
+    //tg.requestFullscreen();
+    //tg.lockOrientation();
 
-    console.log("Telegram WebApp is ready:", tg.initData);
-  } else {
-    console.warn("Telegram WebApp not found.");
-  }
-});//
+    //console.log("Telegram WebApp is ready:", tg.initData);
+  //} else {
+    //console.warn("Telegram WebApp not found.");
+  //}
+//});
 
 
 window.addEventListener("load", () => {
