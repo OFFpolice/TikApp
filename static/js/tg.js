ec9bcc2be90278ef91891a69b782ab04083b1ce3
OFFpolice <offpolice2077@gmail.com>
2025-11-22 10:20:02 +0200

    Update tg.js

--- static/js/tg.js
+++ static/js/tg.js
@@ -1,23 +1,5 @@
-//window.addEventListener("load", () => {
-
-  //if (window.Telegram && window.Telegram.WebApp) {
-    //const tg = window.Telegram.WebApp;
-
-    //tg.ready();
-    //tg.expand();
-    //tg.enableClosingConfirmation();
-    //tg.disableVerticalSwipes();
-    //tg.requestFullscreen();
-    //tg.lockOrientation();
-
-    //console.log("Telegram WebApp is ready:", tg.initData);
-  //} else {
-    //console.warn("Telegram WebApp not found.");
-  //}
-//});
-
-
 window.addEventListener("load", () => {
+
   if (window.Telegram && window.Telegram.WebApp) {
     const tg = window.Telegram.WebApp;
 
@@ -28,18 +10,8 @@ window.addEventListener("load", () => {
     tg.requestFullscreen();
     tg.lockOrientation();
 
-    // === Viewport Fix ===
-    function applyViewportHeight() {
-      const vh = tg.viewportHeight; // уже даёт реальную высоту окна
-      document.documentElement.style.setProperty("--tg-viewport-height", `${vh}px`);
-    }
-
-    // начальная установка
-    applyViewportHeight();
-
-    // обновлять при изменениях
-    tg.onEvent("viewportChanged", applyViewportHeight);
-
-    console.log("TG viewport height applied:", tg.viewportHeight);
+    console.log("Telegram WebApp is ready:", tg.initData);
+  } else {
+    console.warn("Telegram WebApp not found.");
   }
 });
