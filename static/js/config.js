72eed848539dc55efcda8badd96c90e34a5ba67d
OFFpolice <offpolice2077@gmail.com>
2025-12-05 01:42:20 +0200

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -1,4 +1,3 @@
 window.APP_CONFIG = {
-    API_URL_V1: 'https://api.douyin.wtf/api/hybrid/video_data?url={url}&minimal=false',
-    API_URL_V2: 'https://tikwm.com/api/?url={url}'
+    API_URL: 'https://tikwm.com/api/?url={url}'
 };
